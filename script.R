24c8e3f6d34185eadb566ec6e40c8f680343a3bb
Martín González <m@martingonzalez.net>
2018-09-02 16:47:00 +0100

    Better learning rate

--- script.R
+++ script.R
@@ -1,5 +1,6 @@
-library(geojsonio)
+library(sf)
 library(geogrid)
+library(geojsonio)
 
 # Change this according to your folder
 setwd("~/Projects/journocoders-geogrid/")
@@ -11,7 +12,7 @@ par(mfrow = c(2, 3), mar = c(0, 0, 2, 0))
 
 # Hexagonal grid with 6 seeds (takes a while…)
 for (i in 1:6) {
-  grid_hexagon <- calculate_grid(shape = df, grid_type = "hexagonal", seed = i)
+  grid_hexagon <- calculate_grid(shape = df, learning_rate = 0.05, grid_type = "hexagonal", seed = i)
   plot(grid_hexagon, main = paste("Seed", i, sep = " "))
 }
 
@@ -22,7 +23,7 @@ for (i in 1:6) {
 }
 
 # Get a SpatialDataFrame from our desired grid (takes ages…)
-tmp <- calculate_grid(shape = df, grid_type = "hexagonal", seed = 1)
+tmp <- calculate_grid(shape = df, grid_type = "hexagonal", seed = 6)
 df_hex <- assign_polygons(df, tmp)
 
 # And export to TopoJSON
